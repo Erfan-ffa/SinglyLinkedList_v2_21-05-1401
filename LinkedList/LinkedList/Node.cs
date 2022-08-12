376c255211c6d491acb83c5d31a78f0517647c24
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-12 20:27:55 +0430

    Add files via upload

---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int Value;
+        public Node Next;
+    }
+
+    public class NodeOperation
+    {
+        Node head = new Node();
+        Node current;
+
+        public NodeOperation()
+        {
+
+            head.Value = 1;
+            head.Next = null;
+            current = head;
+
+        }
+
+        public void InsertAtLast(int value)
+        {
+
+            Node lastNodeToAdd = new Node();
+            lastNodeToAdd.Value = value;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = lastNodeToAdd;
+
+        }
+
+        public void Print()
+        {
+            for (Node currentNode = head; currentNode != null;)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+
+            //var current = head;
+            //while (current != null)
+            //{
+            //    Console.WriteLine(current.Value);
+            //    current = current.Next;
+            //}
+        }
+    }
+}
